4272cc4bb8ade87db1d07f2ad7d571caa5229cee
lovyan03 <42724151+lovyan03@users.noreply.github.com>
2023-04-20 16:49:42 +0900

    fix compile error

--- src/M5GFX.cpp
+++ src/M5GFX.cpp
@@ -467,7 +467,7 @@ namespace m5gfx
     bus->endTransaction();
     _pin_level(pin_cs, true);
 
-    ESP_LOGD(LIBRARY_NAME, "[Autodetect] read cmd:%02lx = %08lx", cmd, res);
+    ESP_LOGD(LIBRARY_NAME, "[Autodetect] read cmd:%02" PRIx32 " = %08" PRIx32, cmd, res);
     return res;
   }
 
@@ -552,7 +552,7 @@ namespace m5gfx
     {
       if (retry == 1) use_reset = true;
       board = autodetect(use_reset, board);
-      //ESP_LOGD(LIBRARY_NAME,"autodetect board:%d", board);
+      //ESP_LOGD(LIBRARY_NAME,"autodetect board:%" PRIu32, board);
     } while (board_t::board_unknown == board && --retry >= 0);
     _board = board;
 
@@ -571,7 +571,7 @@ namespace m5gfx
 
     if (nvs_board != board) {
       if (0 == nvs_open(LIBRARY_NAME, NVS_READWRITE, &nvs_handle)) {
-        ESP_LOGI(LIBRARY_NAME, "[Autodetect] save to NVS : board:%d", board);
+        ESP_LOGI(LIBRARY_NAME, "[Autodetect] save to NVS : board:%" PRIu32, board);
         nvs_set_u32(nvs_handle, NVS_KEY, board);
         nvs_close(nvs_handle);
       }
@@ -600,7 +600,7 @@ namespace m5gfx
     std::uint32_t id;
 
     std::uint32_t pkg_ver = m5gfx::get_pkg_ver();
-//  ESP_LOGD(LIBRARY_NAME, "pkg_ver : %02x", pkg_ver);
+//  ESP_LOGD(LIBRARY_NAME, "pkg_ver : %02" PRIx32, pkg_ver);
 
     if (pkg_ver == EFUSE_RD_CHIP_VER_PKG_ESP32PICOD4)  /// check PICO-D4 (M5StickC,CPlus,T,T2 / CoreInk / ATOM )
     {
@@ -1005,7 +1005,7 @@ namespace m5gfx
     std::uint32_t id;
 
 //     std::uint32_t pkg_ver = m5gfx::get_pkg_ver();
-// ESP_LOGE("DEBUG","pkg_ver:%02x", pkg_ver);
+// ESP_LOGE("DEBUG","pkg_ver:%02" PRIx32, pkg_ver);
 
       if (board == 0 || board == board_t::board_M5AtomS3)
       {
@@ -1054,8 +1054,8 @@ namespace m5gfx
       {
         lgfx::i2c::init(i2c_port, i2c_sda, i2c_scl);
 
-// ESP_LOGI("DEBUG","AW 0x10 :%02x ", lgfx::i2c::readRegister8(i2c_port, aw9523_i2c_addr, 0x10, 400000).value());
-// ESP_LOGI("DEBUG","AXP0x03 :%02x ", lgfx::i2c::readRegister8(i2c_port, axp_i2c_addr, 0x03, 400000).value());
+// ESP_LOGI("DEBUG","AW 0x10 :%02" PRIx32, lgfx::i2c::readRegister8(i2c_port, aw9523_i2c_addr, 0x10, 400000).value());
+// ESP_LOGI("DEBUG","AXP0x03 :%02" PRIx32, lgfx::i2c::readRegister8(i2c_port, axp_i2c_addr, 0x03, 400000).value());
 
         auto chk_axp = lgfx::i2c::readRegister8(i2c_port, axp_i2c_addr, 0x03, i2c_freq);
         if (chk_axp.has_value() && chk_axp.value() == 0x4A)
