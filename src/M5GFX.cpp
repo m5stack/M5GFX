31bedfcc3463c769cc563f3914b9ea030d47685c
lovyan03 <42724151+lovyan03@users.noreply.github.com>
2025-09-13 23:34:22 +0900

    tweak for Bus_DSI

--- src/M5GFX.cpp
+++ src/M5GFX.cpp
@@ -26,7 +26,10 @@
 
 #if defined ( CONFIG_IDF_TARGET_ESP32P4 )
 
+#include "lgfx/v1/platforms/esp32p4/Bus_DSI.hpp"
 #include "lgfx/v1/platforms/esp32p4/Panel_ILI9881C.hpp"
+#include "lgfx/v1/platforms/esp32p4/Panel_ST7123.hpp"
+#include "lgfx/v1/platforms/esp32p4/Touch_ST7123.hpp"
 
 static constexpr int_fast16_t in_i2c_port = I2C_NUM_1;
 
@@ -1896,14 +1899,14 @@ The usage of each pin is as follows.
 
           static constexpr const uint8_t reg_data_io1_1[] = {
             0x03, 0b01111111, 0,   // PI4IO_REG_IO_DIR
-            0x05, 0b01010110, 0,   // PI4IO_REG_OUT_SET (bit5=GT911 TouchReset LOW)
+            0x05, 0b01000110, 0,   // PI4IO_REG_OUT_SET (bit4=LCD Reset,bit5=GT911 TouchReset  LOW)
             0x07, 0b00000000, 0,   // PI4IO_REG_OUT_H_IM
             0x0D, 0b01111111, 0,   // PI4IO_REG_PULL_SEL
             0x0B, 0b01111111, 0,   // PI4IO_REG_PULL_EN
             0xFF,0xFF,0xFF,
           };
           static constexpr const uint8_t reg_data_io1_2[] = {
-            0x05, 0b01110110, 0,   // PI4IO_REG_OUT_SET (bit5=GT911 TouchReset HIGH)
+            0x05, 0b01110110, 0,   // PI4IO_REG_OUT_SET (bit4=LCD Reset,bit5=GT911 TouchReset  HIGH)
             0xFF,0xFF,0xFF,
           };
 
@@ -1922,49 +1925,101 @@ The usage of each pin is as follows.
           i2c_write_register8_array(in_i2c_port, pi4io2_i2c_addr, reg_data_io2, 400000);
           i2c_write_register8_array(in_i2c_port, pi4io1_i2c_addr, reg_data_io1_2, 400000);
 
-          auto p = new Panel_ILI9881C();
           {
-            auto cfg = p->config();
-            cfg.panel_width = 720;
-            cfg.panel_height = 1280;
-            cfg.memory_width = 720;
-            cfg.memory_height = 1280;
-            cfg.readable = true;
-            cfg.bus_shared = false;
-            cfg.offset_x = 0;
-            cfg.offset_y = 0;
-            cfg.offset_rotation = 0;
-            cfg.pin_cs = GPIO_NUM_NC;
-            cfg.pin_rst = GPIO_NUM_NC;
-            p->config(cfg);
-          }
-          _panel_last.reset(p);
-          _set_pwm_backlight(GPIO_NUM_22, 7, 44100);
+            auto bus_dsi = new Bus_DSI();
+            _bus_last.reset(bus_dsi);
+            auto bus_cfg = bus_dsi->config();
+            bus_cfg.bus_id = 0;
+            bus_cfg.lane_num = 2;
+            // bus_cfg.lane_mbps = 730;
+            bus_cfg.lane_mbps = 965;
+            bus_cfg.ldo_chan_id = 3;
+            bus_cfg.ldo_voltage_mv = 2500;
+            bus_dsi->config(bus_cfg);
+            if (bus_dsi->init()) {
+              bool hit_ili9881 = false;
+              for (int i = 0; i < 3; ++i) {
+                lgfx::delay(4);
+                static constexpr uint8_t params[] = { 0x98, 0x81, 0x01 };
+                bus_dsi->writeParams( 0xFF, params, 3);
+                uint8_t id1 = 0, id2 = 0, id3 = 0;
+                bus_dsi->readParams( 0x00, &id1, 1 );
+                bus_dsi->readParams( 0x01, &id2, 1 );
+                bus_dsi->readParams( 0x02, &id3, 1 );
+                ESP_LOGV(LIBRARY_NAME, "DSI ID %02x %02x %02x", id1, id2, id3);
+                if (id1 == 0x98 && id2 == 0x81) {
+                  hit_ili9881 = true;
+                  break;
+                }
+              }
+              if (hit_ili9881) {
+                _touch_last.reset(new Touch_GT911());
+                auto p = new Panel_ILI9881C();
+                _panel_last.reset(p);
+                auto det = p->config_detail();
+                det.dpi_freq_mhz = 60;
+                det.hsync_back_porch = 140;
+                det.hsync_pulse_width = 40;
+                det.hsync_front_porch = 40;
+                det.vsync_back_porch = 20;
+                det.vsync_pulse_width = 4;
+                det.vsync_front_porch = 20;
+                p->config_detail(det);
+              } else {
+                _touch_last.reset(new Touch_ST7123());
+                auto p = new Panel_ST7123();
+                _panel_last.reset(p);
+                auto det = p->config_detail();
+                det.dpi_freq_mhz = 70;
+                det.hsync_back_porch = 40;
+                det.hsync_pulse_width = 2;
+                det.hsync_front_porch = 40;
+                det.vsync_back_porch = 8;
+                det.vsync_pulse_width = 2;
+                det.vsync_front_porch = 220;
+                p->config_detail(det);
+              }
+              {
+                auto p = _panel_last.get();
+                auto cfg = p->config();
+                cfg.memory_width = 720;
+                cfg.memory_height = 1280;
+                cfg.panel_width = 720;
+                cfg.panel_height = 1280;
+                cfg.readable = true;
+                cfg.rgb_order = true;
+                cfg.bus_shared = false;
+                cfg.offset_x = 0;
+                cfg.offset_y = 0;
+                cfg.offset_rotation = 0;
+                cfg.pin_cs = GPIO_NUM_NC;
+                cfg.pin_rst = GPIO_NUM_NC;
+                p->config(cfg);
+                p->setBus(bus_dsi);
+              }
+              auto t = _touch_last.get();
+              if (t) {
+                auto cfg = t->config();
 
-          {
-            auto t = new m5gfx::Touch_GT911();
-            if (t) {
-              _touch_last.reset(t);
-
-              auto cfg = t->config();
-
-              cfg.pin_rst = -1;
-              cfg.pin_sda = GPIO_NUM_31;
-              cfg.pin_scl = GPIO_NUM_32;
-              cfg.pin_int = GPIO_NUM_23;
-              cfg.freq = 400000;
-              cfg.x_min = 0;
-              cfg.x_max = 719;
-              cfg.y_min = 0;
-              cfg.y_max = 1279;
-              cfg.i2c_port = 1;
-              cfg.bus_shared = false;
-              cfg.offset_rotation = 0;
-              t->config(cfg);
-
-              _panel_last->touch(t);
+                cfg.pin_rst = -1;
+                cfg.pin_sda = GPIO_NUM_31;
+                cfg.pin_scl = GPIO_NUM_32;
+                cfg.pin_int = GPIO_NUM_23;
+                cfg.freq = 400000;
+                cfg.x_min = 0;
+                cfg.x_max = 719;
+                cfg.y_min = 0;
+                cfg.y_max = 1279;
+                cfg.i2c_port = 1;
+                cfg.bus_shared = false;
+                cfg.offset_rotation = 0;
+                t->config(cfg);
+                _panel_last->setTouch(t);
+              }
             }
           }
+          _set_pwm_backlight(GPIO_NUM_22, 7, 44100);
+
 
           goto init_clear;
         }
