cd9dc75cb7742c41c883bee54870aa0508bcc1ee
ainyan03 <ainyan03@users.noreply.github.com>
2026-06-24 18:06:33 +0900

    Add M5ToughC5 board support with Touch_CHSC6540 and Panel_StopWatch
    New board - M5ToughC5 (ESP32-C5):
    - Autodetect via M5PM1 (0x6E) + M5IOE1 (0x4F) on I2C0 (SDA=2, SCL=3)
    - ILI9342c 320x240 SPI LCD (CS=25, DC=26, MOSI=7, MISO=8, SCLK=9)
    - Backlight/reset/enable via M5IOE1 I/O expander
    - Touch reset via M5PM1 GPIO2
    - board_M5ToughC5 added to boards.hpp
    - SDL simulator title entry added
    
    Touch_CHSC6540 driver (new):
    - Chipsemi CHSC6540 capacitive touch controller (I2C addr 0x2E)
    - 12-bit coordinates, up to 2 touch points
    - Replaces inline Touch_M5Tough / Touch_M5ToughC5 in M5GFX.cpp
    - Used by both M5Tough (ESP32) and M5ToughC5 (ESP32-C5)
    
    Panel_StopWatch:
    - Inherits Panel_CO5300 with M5StopWatch-specific init commands (480x480)
    
    Verified on M5ToughC5 V0.6 hardware with IDF v5.5.4.

---
+++ src/lgfx/v1/touch/Touch_CHSC6540.hpp
@@ -0,0 +1,48 @@
+/*----------------------------------------------------------------------------/
+  Lovyan GFX - Graphics library for embedded devices.
+
+Original Source:
+ https://github.com/lovyan03/LovyanGFX/
+
+Licence:
+ [FreeBSD](https://github.com/lovyan03/LovyanGFX/blob/master/license.txt)
+
+Author:
+ [lovyan03](https://twitter.com/lovyan03)
+
+Contributors:
+ [ciniml](https://github.com/ciniml)
+ [mongonta0716](https://github.com/mongonta0716)
+ [tobozo](https://github.com/tobozo)
+/----------------------------------------------------------------------------*/
+#pragma once
+
+#include "../Touch.hpp"
+
+namespace lgfx
+{
+ inline namespace v1
+ {
+//----------------------------------------------------------------------------
+
+  struct Touch_CHSC6540 : public ITouch
+  {
+    Touch_CHSC6540(void)
+    {
+      _cfg.i2c_addr = 0x2e;
+      _cfg.x_min = 0;
+      _cfg.x_max = 319;
+      _cfg.y_min = 0;
+      _cfg.y_max = 239;
+    }
+
+    bool init(void) override;
+    void wakeup(void) override {}
+    void sleep(void) override {}
+
+    uint_fast8_t getTouchRaw(touch_point_t* tp, uint_fast8_t count) override;
+  };
+
+//----------------------------------------------------------------------------
+ }
+}
